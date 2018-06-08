aa9c32323262c95af99cbf6743e09fcd6b17bb88
nwestberg <39102347+nwestberg@users.noreply.github.com>
2018-06-08 09:49:12 +0200

    Update osx_tweaks.sh

--- osx_tweaks.sh
+++ osx_tweaks.sh
@@ -18,3 +18,8 @@ defaults write -g NSInitialToolTipDelay -int 100
 # Silent reboot (removes the reboot chime sound)
 # sudo nvram SystemAudioVolume=%80
 
+# Use p4merge as git diff tool
+# git config --global diff.tool p4merge
+# git config --global difftool.p4merge.cmd '/Applications/p4merge.app/Contents/MacOS/p4merge $LOCAL $REMOTE'
+# git config --global difftool.prompt false
+
